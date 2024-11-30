3ec7660c413db58d084b3020156fd2a8c35917a6
TNTwise <77559100+TNTwise@users.noreply.github.com>
2024-11-30 18:55:17 +0000

    Update bump torch to 2.6.0-dev20241130
    Former-commit-id: e318f62c775985ee8e40cf9fb7e0f71f1568a724
    Former-commit-id: 798abedf650c45820d693b5234c5d3feb76e1555

--- src/DownloadDeps.py
+++ src/DownloadDeps.py
@@ -215,8 +215,8 @@ class DownloadDependencies:
         Pytorch CUDA deps
         """
         torchCUDADeps = [
-            "torch==2.6.0.dev20241023",
-            "torchvision==0.20.0.dev20241023",
+            "torch==2.6.0.dev20241130",
+            "torchvision==0.20.0.dev20241130",
             "safetensors",
             "einops",
             "cupy-cuda12x==13.3.0",
@@ -236,7 +236,7 @@ class DownloadDependencies:
             "tensorrt-cu12_libs==10.6.0",
             "tensorrt_cu12_bindings==10.6.0",
             "--no-deps",
-            "torch_tensorrt==2.6.0.dev20241023",
+            "torch_tensorrt==2.6.0.dev20241130",
         ]
 
         return tensorRTDeps
