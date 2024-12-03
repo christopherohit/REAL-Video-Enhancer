d7a2f5701bdddcb36890c5f35f2bead24f60a95c
TNTwise <tntwise3@gmail.com>
2024-12-03 17:05:17 -0600

    update backend link
    Former-commit-id: 40f0f5433b0e37755b04b45ae353ef12fdc4f823
    Former-commit-id: bceac17b6ad716a15d1877b1840fca7bbb5ff0ba

--- backend/src/ncnn/InterpolateNCNN.py
+++ backend/src/ncnn/InterpolateNCNN.py
@@ -87,67 +87,6 @@ class Rife:
         else:
             raise FileNotFoundError(f"{model_dir} not found")
 
-    def process(self, image0: Image, image1: Image, timestep: float = 0.5) -> Image:
-        # Return the image immediately instead of doing the copy in the upstream part which cause black output problems
-        # The reason is that the upstream code use ncnn::Mat::operator=(const Mat& m) does a reference copy which won't
-        # change our OutImage data.
-        if timestep == 0.0:
-            return image0
-        elif timestep == 1.0:
-            return image1
-
-        image0_bytes = bytearray(image0.tobytes())
-        image1_bytes = bytearray(image1.tobytes())
-        channels = int(len(image0_bytes) / (image0.width * image0.height))
-        output_bytes = bytearray(len(image0_bytes))
-
-        # convert image bytes into ncnn::Mat Image
-        raw_in_image0 = wrapped.Image(
-            image0_bytes, image0.width, image0.height, channels
-        )
-        raw_in_image1 = wrapped.Image(
-            image1_bytes, image1.width, image1.height, channels
-        )
-        raw_out_image = wrapped.Image(
-            output_bytes, image0.width, image0.height, channels
-        )
-
-        self._rife_object.process(raw_in_image0, raw_in_image1, timestep, raw_out_image)
-        return Image.frombytes(
-            image0.mode, (image0.width, image0.height), bytes(output_bytes)
-        )
-
-    def process_cv2(
-        self, image0: np.ndarray, image1: np.ndarray, timestep: float = 0.5
-    ) -> np.ndarray:
-        if timestep == 0.0:
-            return image0
-        elif timestep == 1.0:
-            return image1
-
-        image0_bytes = bytearray(image0.tobytes())
-        image1_bytes = bytearray(image1.tobytes())
-
-        self.channels = int(len(image0_bytes) / (image0.shape[1] * image0.shape[0]))
-        self.output_bytes = bytearray(len(image0_bytes))
-
-        # convert image bytes into ncnn::Mat Image
-        raw_in_image0 = wrapped.Image(
-            image0_bytes, image0.shape[1], image0.shape[0], self.channels
-        )
-        raw_in_image1 = wrapped.Image(
-            image1_bytes, image0.shape[1], image0.shape[0], self.channels
-        )
-        raw_out_image = wrapped.Image(
-            self.output_bytes, image0.shape[1], image0.shape[0], self.channels
-        )
-
-        self._rife_object.process(raw_in_image0, raw_in_image1, timestep, raw_out_image)
-
-        return np.frombuffer(self.output_bytes, dtype=np.uint8).reshape(
-            image0.shape[0], image0.shape[1], self.channels
-        )
-
     def patch_pause(self):
         """
         Used in instances where the scene change is active, and the frame needs to be uncached.
@@ -180,65 +119,7 @@ class Rife:
         if timestep == self.max_timestep:
             self.image0_bytes = self.image1_bytes
             self.raw_in_image0 = self.raw_in_image1
-
         return bytes(self.output_bytes)
-
-    def process_fast(
-        self,
-        image0: np.ndarray,
-        image1: np.ndarray,
-        timestep: float = 0.5,
-        shape: tuple = None,
-        channels: int = 3,
-    ) -> np.ndarray:
-        """
-        An attempt at a faster implementation for NCNN that should speed it up significantly through better caching methods.
-
-        :param image0: The first image to be processed.
-        :param image1: The second image to be processed.
-        :param timestep: The timestep value for the interpolation.
-        :param shape: The shape of the images.
-        :param channels: The number of channels in the images.
-
-        :return: The processed image, format: np.ndarray.
-        """
-
-        if timestep == 0.0:
-            return np.array(image0)
-        elif timestep == 1.0:
-            return np.array(image1)
-
-        if self.height == None:
-            if shape is None:
-                self.height, self.width, self.channels = image0.shape
-            else:
-                self.height, self.width = shape
-
-        image1_bytes = bytearray(image1.tobytes())
-        raw_in_image1 = wrapped.Image(image1_bytes, self.width, self.height, channels)
-
-        if self.image0_bytes is None:
-            self.image0_bytes = bytearray(image0.tobytes())
-            self.output_bytes = bytearray(len(self.image0_bytes))
-
-        raw_in_image0 = wrapped.Image(
-            self.image0_bytes, self.width, self.height, channels
-        )
-
-        raw_out_image = wrapped.Image(
-            self.output_bytes, self.width, self.height, channels
-        )
-
-        self._rife_object.process(raw_in_image0, raw_in_image1, timestep, raw_out_image)
-
-        self.image0_bytes = image1_bytes
-
-        return np.frombuffer(self.output_bytes, dtype=np.uint8).reshape(
-            self.height, self.width, self.channels
-        )
-
-
-class RIFE(Rife): ...
 
 
 class InterpolateRIFENCNN:
