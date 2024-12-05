ea870c9c03eeb45af94395cdce7c889f555e3d10
TNTwise <tntwise3@gmail.com>
2024-12-04 19:58:22 -0600

    remove context manager in discord rpc
    Former-commit-id: aa3a4320782f99803d32e3e76164c26b77d43b3e
    Former-commit-id: 81ffe23da176253cfd0adf68b7d9344200e27393

--- src/DiscordRPC.py
+++ src/DiscordRPC.py
@@ -1,30 +1,10 @@
-from pypresence import Presence, exceptions
-from contextlib import contextmanager
+from pypresence import Presence
 import os
 from .Util import log, networkCheck
-import threading
-
-
-class TimeoutException(Exception):
-    pass
-
-
-@contextmanager
-def time_limit(seconds):
-    def timeout_handler():
-        raise TimeoutException("Timed out!")
-
-    timer = threading.Timer(seconds, timeout_handler)
-    timer.start()
-    try:
-        yield
-    finally:
-        timer.cancel()
 
 
 class DiscordRPC:
     def start_discordRPC(self, mode: str, videoName: str, backend: str):
-        return # Disable Discord RPC, it is shit
         if networkCheck():
             """
             Attempts to connect to discord for RPC suppor
@@ -33,32 +13,31 @@ class DiscordRPC:
                 videoName (str): The name of the video
             """
             try:
-                with time_limit(2):
-                    os.system(
-                        "ln -sf {app/com.discordapp.Discord,$XDG_RUNTIME_DIR}/discord-ipc-0"
-                    )  # Enables discord RPC on flatpak
-                    client_id = "1278176380043132961"  # ID for rpc
-                    try:
-                        for i in range(10):
-                            ipc_path = f"{os.getenv('XDG_RUNTIME_DIR')}/discord-ipc-{i}"
-                            if not os.path.exists(ipc_path) or not os.path.isfile(ipc_path):
-                                os.symlink(
-                                    f"{os.getenv('HOME')}/.config/discord/{client_id}",
-                                    ipc_path,
-                                )
-                    except Exception:
-                        log("Not flatpak")
-                    try:
-                        self.RPC = Presence(client_id)  # Initialize the client class
-                        self.RPC.connect()  # Start the handshake loop
+                os.system(
+                    "ln -sf {app/com.discordapp.Discord,$XDG_RUNTIME_DIR}/discord-ipc-0"
+                )  # Enables discord RPC on flatpak
+                client_id = "1278176380043132961"  # ID for rpc
+                try:
+                    for i in range(10):
+                        ipc_path = f"{os.getenv('XDG_RUNTIME_DIR')}/discord-ipc-{i}"
+                        if not os.path.exists(ipc_path) or not os.path.isfile(ipc_path):
+                            os.symlink(
+                                f"{os.getenv('HOME')}/.config/discord/{client_id}",
+                                ipc_path,
+                            )
+                except Exception:
+                    log("Not flatpak")
+                try:
+                    self.RPC = Presence(client_id)  # Initialize the client class
+                    self.RPC.connect()  # Start the handshake loop
 
-                        self.RPC.update(
-                            state=f"{mode} Video",
-                            details=f"Backend: {backend}",
-                            large_image="logo-v2",
-                        )
-                    except Exception:
-                        pass
+                    self.RPC.update(
+                        state=f"{mode} Video",
+                        details=f"Backend: {backend}",
+                        large_image="logo-v2",
+                    )
+                except Exception:
+                    pass
 
             # The presence will stay on as long as the program is running
             # Can only update rich presence every 15 seconds
