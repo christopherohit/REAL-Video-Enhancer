c33a8a17ba69cdad7810737a923824d89527e8dd
TNTwise <77559100+TNTwise@users.noreply.github.com>
2024-12-28 16:03:06 -0600

    Update rve-backend.py
    Former-commit-id: 16c36bfe103aa695fb3c5bed0d88163ccb074dec
    Former-commit-id: 52b2d751633ccbfa21903490aef1b9a4719f2c18

--- backend/rve-backend.py
+++ backend/rve-backend.py
@@ -210,7 +210,7 @@ class HandleApplication:
             "--video_encoder_preset",
             help="encoder preset that sets default encoder settings useful for hardware encoders.",
             default="libx264",
-            choices=["libx264", "libx265", "vp9", "av1", "x264_vulkan", "x264_nvenc", "x265_nvenc"],
+            choices=["libx264", "libx265", "vp9", "av1", "x264_vulkan", "x264_nvenc", "x265_nvenc", "av1_nvenc"],
             type=str,
         )
 
